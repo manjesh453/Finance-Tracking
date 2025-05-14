956041c5b00620bf62af3457ed159e12a1fe0cfa
manjesh453 <rayamajhimanjesh41@gmail.com>
2025-05-14 22:19:57 +0545

    Cors changes on

--- api-gateway/src/main/java/com/apigateway/config/Beans.java
+++ api-gateway/src/main/java/com/apigateway/config/Beans.java
@@ -3,6 +3,9 @@ package com.apigateway.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.cloud.gateway.config.GlobalCorsProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,5 +19,19 @@ public class Beans {
                         .title("Microservices API")
                         .description("API Documentation for all Microservices")
                         .version("1.0.0"));
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("example_route", r -> r.path("/example/**")
+                        .uri("http://localhost:8081")
+                )
+                .build();
+    }
+
+    @Bean
+    public GlobalCorsProperties globalCorsProperties() {
+        return new GlobalCorsProperties();
     }
 }
