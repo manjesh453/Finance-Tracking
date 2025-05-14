1e72d66d4502330a904c2e03043f5615e701c360
manjesh453 <rayamajhimanjesh41@gmail.com>
2025-05-14 15:19:33 +0545

    Cors config

---
+++ expenses/src/main/java/com/expenses/conf/CorsConfigurations.java
@@ -0,0 +1,29 @@
+package com.expenses.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+
+@Configuration
+public class CorsConfigurations {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Explicit origin
+        configuration.setAllowCredentials(true); // Allows cookies/auth headers
+        configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+}
