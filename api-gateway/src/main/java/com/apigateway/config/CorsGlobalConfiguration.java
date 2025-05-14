b7defb11b3a76df6e3eb52a44068b9f2f0474713
manjesh453 <rayamajhimanjesh41@gmail.com>
2025-05-14 21:52:36 +0545

    Cors changes on

--- api-gateway/src/main/java/com/apigateway/config/CorsGlobalConfiguration.java
+++ api-gateway/src/main/java/com/apigateway/config/CorsGlobalConfiguration.java
@@ -1,32 +1,33 @@
-package com.apigateway.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsGlobalConfiguration {
-
-    private static final String GET = "GET";
-    private static final String POST = "POST";
-    private static final String DELETE = "DELETE";
-    private static final String PUT = "PUT";
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                WebMvcConfigurer.super.addCorsMappings(registry);
-                registry.addMapping("/**")
-                        .allowedMethods(GET, POST, PUT, DELETE)
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedHeaders("*")
-                        .allowedOriginPatterns("*")
-                        .allowCredentials(true)
-                ;
-            }
-        };
-    }
-}
+//package com.apigateway.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsGlobalConfiguration {
+//
+//    private static final String GET = "GET";
+//    private static final String POST = "POST";
+//    private static final String DELETE = "DELETE";
+//    private static final String PUT = "PUT";
+//    private static final String OPTION = "OPTION";
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                WebMvcConfigurer.super.addCorsMappings(registry);
+//                registry.addMapping("/**")
+//                        .allowedMethods(GET, POST, PUT, DELETE,OPTION)
+//                        .allowedOrigins("http://localhost:3000")
+//                        .allowedHeaders("*")
+//                        .allowedOriginPatterns("*")
+//                        .allowCredentials(true)
+//                ;
+//            }
+//        };
+//    }
+//}
